afa8527341c0338c59d1d0bbb430c90abab595a3
ppadole2025 <ppadole2025@gmail.com>
2026-05-30 15:31:01 +0530

    add the testcase

---
+++ src/test/java/tests/LoginTestPP.java
@@ -0,0 +1,33 @@
+package tests;
+
+import base.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+
+public class LoginTestPP extends BaseTest {
+
+    @Test(priority = 1)
+    public void verifyLoginp1() {
+
+        LoginPage lp = new LoginPage(driver);
+
+        lp.login("test@gmail.com", "test123");
+    }
+    @Test(priority = 2)
+    public void verifyLoginp2() {
+
+        LoginPage lp = new LoginPage(driver);
+
+        lp.login("test@gmail.com", "test1234");
+        Assert.assertEquals(5,6,"Forcefully fail");
+    }
+    @Test(priority = 3)
+    public void verifyLoginp3() {
+
+        LoginPage lp = new LoginPage(driver);
+
+        lp.login("test@gmail.com", "test12345");
+    }
+
+}
